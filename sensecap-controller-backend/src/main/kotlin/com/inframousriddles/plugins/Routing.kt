ae98b2bfa752b429bfb38394de5f4ca1a3e6c7ec
pavlospt <p.tournaris@gmail.com>
2021-11-23 21:37:44 +0200

    Add DockerCompose
    * In order to expose a port on Balena, we need to utilize
    DockerCompose to have a proper port mapping to our service.

--- sensecap-controller-backend/src/main/kotlin/com/inframousriddles/plugins/Routing.kt
+++ sensecap-controller-backend/src/main/kotlin/com/inframousriddles/plugins/Routing.kt
@@ -5,20 +5,12 @@ import com.inframousriddles.sensecap.routes.SenseCAPRoutes
 import io.ktor.client.*
 import io.ktor.client.engine.jetty.*
 import io.ktor.server.routing.*
-import io.ktor.serialization.kotlinx.json.*
 import io.ktor.server.plugins.*
 import io.ktor.server.application.*
 import io.ktor.server.response.*
 
 fun Application.configureRouting() {
     install(DoubleReceive)
-
-    install(HttpsRedirect) {
-        // The port to redirect to. By default, 443, the default HTTPS port.
-        sslPort = 443
-        // 301 Moved Permanently, or 302 Found redirect.
-        permanentRedirect = false
-    }
 
     val client = HttpClient(Jetty)
 
